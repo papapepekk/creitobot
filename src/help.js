93f0a767beda0070503457306591c07582fcc6a3
papapepekk <77702436+papapepekk@users.noreply.github.com>
2021-02-04 23:02:40 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,141 @@
+const help = (prefix) => { 
+	return `                 
+<══════════════════════>
+            CREITIM - BOT <══════════════════════>
+
+__█████████         
+__█▄█████▄█   Author:ELɢᴜsᴛᴀ
+__█▼▼▼▼▼█ 
+_██CREITIM██▌ 
+__█▲▲▲▲▲█ 
+__█████████ 
+____██_____██
+
+            M E N U  B O T
+╔══════════════════════
+║       LISTA DE COMANDOS
+╠══════════════════════
+║   COMANDOS ᏢᎪᎡᎪ ᎪᎠᎷ'Տ
+║╭─────────────────────
+║├> ${prefix}welcome [0/1]
+║├> ${prefix}bv [0/1]
+║├> ${prefix}bemvindo [0/1]
+║├> ${prefix}boasvindas [0/1]
+║├> ${prefix}add 55xxx
+║├> ${prefix}adicionar 55xxx
+║├> ${prefix}kick @membro
+║├> ${prefix}ban @membro
+║├> ${prefix}remover @membro
+║├> ${prefix}promover @membro
+║├> ${prefix}demote @adm
+║├> ${prefix}rebaixar @adm
+║├> ${prefix}grupo abrir/fechar
+║├> ${prefix}mudarnome seu txt(muda nome do gp)
+║├> ${prefix}listaadms
+║├> ${prefix}listadmins
+║├> ${prefix}adms
+║├> ${prefix}setdesc seu txt
+║├> ${prefix}mudardesc seu txt
+║├> ${prefix}mudarfoto
+║├> ${prefix}marcar
+
+║├> MIDIA/OUTROS
+
+║├─────────────────────
+       FOTO LOFI(única)
+║├>${prefix}lofi
+║├─────────────────────
+       TIK TOK
+║├> ${prefix}tiktokstalk (Nick da conta)
+║├> ${prefix}fototiktok
+║├─────────────────────
+     BAIXA VIDEO DO YOUTUBE
+║├> ${prefix}ytmp4 link
+║├> ${prefix}ytvideo link
+║├> ${prefix}ytbuscar link
+║├> ${prefix}ytbaixa link
+║├> ${prefix}play
+║├─────────────────────
+    TEMPORARIAMENTE OFF
+║├> ${prefix}sticker|||||||||||||||||
+║├> ${prefix}stiker |||||||||||||||||
+║├> ${prefix}fig    |||||||||||||||||
+║├> ${prefix}figurinha|||||||||||||||
+║├────> fazer fig cm foto/gif|||||
+....................................
+║├─────────────────────
+      CONVERTE TEXTO EM ÁUDIO
+║├> ${prefix}tts seu txt
+║├> ${prefix}gtts seu txt
+║├> ${prefix}audio seu txt
+║├─────────────────────
+    CONVERTE FIGURINHA EM IMAGEM
+║├> ${prefix}toimg marca fig
+║├> ${prefix}converter marca fig
+║├─────────────────────
+     PEGA O TEXTO DA FOTO
+║├> ${prefix}txtdafoto
+║├> ${prefix}ocr
+║├─────────────────────
+      PESQUISA NOME DO ANIME
+║├> ${prefix}wait marca foto
+║├> ${prefix}pesquisar marca foto
+║├─────────────────────
+    FOTO DE DOG ALEATÓRIO
+║├> ${prefix}cachorro
+║├> ${prefix}auau
+║├> ${prefix}dog
+║├─────────────────────
+      POKEMON ALEATÓRIO
+║├> ${prefix}pokemon
+║├─────────────────────
+       LOGO COM SEU TEXTO(off)
+║├> ${prefix}logo3d seu txt
+║├> ${prefix}logoph seu|txt
+║├─────────────────────
+      PEGA FOTO DO LOCAL
+║├> ${prefix}map 0000-0000(cep) 
+║├> ${prefix}mapa 0000-0000(cep)
+║├─────────────────────
+
+      INFORMAÇÕES DO BOT
+║├> ${prefix}info
+║├> ${prefix}listabloq
+║├─────────────────────
+
+       ᏟϴᎷᎪΝᎠϴՏ Ꭰϴ ᏟᎡᏆᎪᎠϴᎡ
+║├─────────────────────
+║├> ${prefix}hidetag
+║├> ${prefix}clearall
+║├> ${prefix}limpar
+║├> ${prefix}bloquear @membro
+║├> ${prefix}desbloquear
+║├> ${prefix}kitar
+║├> ${prefix}bc
+║├> ${prefix}clonar
+║├> ${prefix}clone
+║├> ${prefix}sigla (nova sigla)
+
+             ՏႮᏢϴᎡͲᎬ
+║├─────────────────────
+        BUG REPORT
+║├> ${prefix}bug
+║├> ${prefix}reportar
+║├> ${prefix}reportarbug
+
+  se quiser falar com meu dono chame ele nesse numero: wa.me/+5561993845817
+║└─────────────────────
+╚══════════════════════
+
+           C R E I T I M  B O T
+
+     ┌─────────😎────────┐
+              sʜɪᴛs_ᴍᴀɴɢᴀ_ʀᴏsᴀ 
+     └─────────😎────────┘
+
+`
+}
+exports.help = help
+
+
+  
